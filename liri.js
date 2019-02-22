93ea8bf2740a006531e15020cd61189d21a82477
thedanielenrico <thedanielenrico@gmail.com>
2019-02-22 12:16:00 -0800

    Put in if statement to handle movie-this without a title

--- liri.js
+++ liri.js
@@ -11,27 +11,38 @@ var spotify = new Spotify(keys.spotify);
 
 
 var movieName = "";
-for (var i = 3; i < process.argv.length; i++){
+for (var i = 3; i < process.argv.length; i++) {
     movieName = movieName + "+" + process.argv[i];
 }
 if (process.argv[2] === "movie-this") {
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-    axios.get(queryUrl).then(
-        function (response) {
-            console.log("Title: " + response.data.Title);
-            console.log("Release year: " + response.data.Year);
-            console.log("The movie's IMDB rating is: " + response.data.imdbRating);
-            // console.log("The movie's Rotten Tomatoes rating is: " + response.data.Ratings[1].Value);
-            console.log("Production: " + response.data.Production);
-            console.log("Language: " + response.data.Language);
-            console.log("Plot summary: " + response.data.Plot);
-            
-            
+    if (movieName === "") {
+        var queryUrl = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy";
+        axios.get(queryUrl).then(
+            function (response) {
+                console.log("Title: " + response.data.Title);
+                console.log("Release year: " + response.data.Year);
+                console.log("The movie's IMDB rating is: " + response.data.imdbRating);
+                console.log("Production: " + response.data.Production);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot summary: " + response.data.Plot);
+                console.log(queryUrl)
+            }
+        );
 
+    } else {
 
-
-
-            console.log(queryUrl)
-        }
-    );
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+        axios.get(queryUrl).then(
+            function (response) {
+                console.log("Title: " + response.data.Title);
+                console.log("Release year: " + response.data.Year);
+                console.log("The movie's IMDB rating is: " + response.data.imdbRating);
+                // console.log("The movie's Rotten Tomatoes rating is: " + response.data.Ratings[1].Value);
+                console.log("Production: " + response.data.Production);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot summary: " + response.data.Plot);
+                console.log(queryUrl)
+            }
+        );
+    }
 }
