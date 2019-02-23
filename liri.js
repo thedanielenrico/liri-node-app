8c720fd524860d36979de45222d6afbe59a74729
thedanielenrico <thedanielenrico@gmail.com>
2019-02-23 10:14:33 -0800

    Start working on Spotify API

--- liri.js
+++ liri.js
@@ -1,19 +1,43 @@
 var fs = require("fs");
 var Spotify = require('node-spotify-api');
 var axios = require("axios");
-
-
-
 require("dotenv").config();
-
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
 
-var movieName = "";
-for (var i = 3; i < process.argv.length; i++) {
+
+
+
+
+var bandName = process.argv[3]
+for (var i = 4; i < process.argv.length; i++) {
+    bandName = bandName + "+" + process.argv[i];
+}
+
+
+var movieName = process.argv[3];
+for (var i = 4; i < process.argv.length; i++) {
     movieName = movieName + "+" + process.argv[i];
 }
+// Spotify API
+if (process.argv[2] === "spotify-this-song") {
+    spotify.search({
+        type: 'track', query: process.argv[3],
+        limit: 10
+    },
+        function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            // console.log(data);
+            // console.log(data.tracks.items[0]);
+            // console.log("Artist: " + data.tracks.items[0].artists.name)
+            console.log("Album: " + data.tracks.items[0].album)
+
+        });
+}
+// OMDB instance
 if (process.argv[2] === "movie-this") {
     if (movieName === "") {
         var queryUrl = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy";
@@ -31,13 +55,12 @@ if (process.argv[2] === "movie-this") {
 
     } else {
 
-        var queryUrl = "http://www.omdbapi.com/?t=" + process.argv[3] + "&y=&plot=short&apikey=trilogy";
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
         axios.get(queryUrl).then(
             function (response) {
                 console.log("Title: " + response.data.Title);
                 console.log("Release year: " + response.data.Year);
                 console.log("The movie's IMDB rating is: " + response.data.imdbRating);
-                // console.log("The movie's Rotten Tomatoes rating is: " + response.data.Ratings[1].Value);
                 console.log("Production: " + response.data.Production);
                 console.log("Language: " + response.data.Language);
                 console.log("Plot summary: " + response.data.Plot);
@@ -46,18 +69,14 @@ if (process.argv[2] === "movie-this") {
         );
     }
 }
-var bandName = "";
-for (var i = 3; i < process.argv.length; i++) {
-    bandName = bandName + "+" + process.argv[i];
-}
-if (process.argv[2] === "concert-this"){
-    var queryUrl = "https://rest.bandsintown.com/artists/" + process.argv[3] + "/events?app_id=codingbootcamp";
-        axios.get(queryUrl).then(
-            function (response) {
-                console.log(response.data[0].venue.name)
-                console.log(response.data[0].datetime)
-                
-                console.log(queryUrl)
-            }
-        );
+// Bands in Town instance
+if (process.argv[2] === "concert-this") {
+    var queryUrl = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp";
+    axios.get(queryUrl).then(
+        function (response) {
+            console.log(response.data[0].venue.name)
+            console.log(response.data[0].datetime)
+            console.log(queryUrl)
+        }
+    );
 }
